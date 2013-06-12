5ec65d095948d8a9ce54dfb47fdae9489514108e
brian-frichette <brian.frichette@gmail.com>
2013-06-12 10:45:22 -0700

    Progress on loading

--- public/javascripts/lazy-loading.js
+++ public/javascripts/lazy-loading.js
@@ -1,11 +1,12 @@
 (function(){
-	var cardSlider = Hammer($('card-slider'))
+	var cardSlider = Hammer($('card-slider'), { prevent_default: true })
 		, slider = cardSlider.element
 		, posX = 0, prevPosX = 0;
 
 	cardSlider.on('drag release swipe', function(evt){
 		switch (evt.type) {
 			case 'drag':
+				slider.removeClass('slider-transition');
 				posX = evt.gesture.deltaX + prevPosX;
 				translateX(slider, posX);
 				break;
@@ -25,18 +26,52 @@
 		slider.removeClass('slide-transition');
 	});
 
+	// Add active to cards
+	window.cards.forEach(function(card){ card.active = false; });
+	var cardFullWidth = 0;
+
 	function CardViewModel() {
 		var self = this;
 		window.addEvent('resize', function(){
 			self.windowWidth(window.innerWidth);
 		});
 
+		this.loaderUrl = "public/img/loader.gif";
 		this.cards = ko.observableArray(window.cards);
 		this.windowWidth = ko.observable(window.innerWidth);
+		this.totalCards = function() { return this.cards().length; };
+
+		this.totalVisibleCards = function() {
+			return Math.floor(this.windowWidth() / this.cardWidth);
+		};
+
+		this.getCardInfo = function(el) {
+			if (!this.cardWidth) {
+				this.cardWidth = el.offsetWidth + parseFloat(el.getComputedStyle('margin-right'));
+				var total = this.totalVisibleCards();
+				this.startIdx = -Math.round(posX / this.cardWidth);
+				this.endIdx = this.startIdx + total;
+				this.endIdx = (this.endIdx > this.totalCards() ? this.totalCards() : this.endIdx);
+			}
+			return this.cardWidth;
+		};
+
 		this.goBack = function() {
 			if (posX === 0) return;
 			slider.addClass('slide-transition');
 			translateX(slider, 0);
+		};
+
+		this.isActive = function(idx) {
+			if (idx >= this.startIdx && idx <= this.endIdx) {
+				return true;
+			}
+
+			return false;
+		};
+
+		this.cardImageUrl = function(card, idx) {
+			return this.isActive(idx()) ? card.categoryImageUrl : this.loaderUrl;
 		};
 	}
 
