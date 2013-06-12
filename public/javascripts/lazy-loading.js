e6ee9e1bc7f4ac8f517b645a0f3582c2f250b49b
brian-frichette <brian.frichette@gmail.com>
2013-06-12 11:35:32 -0700

    Updates to behavior and loading

--- public/javascripts/lazy-loading.js
+++ public/javascripts/lazy-loading.js
@@ -1,21 +1,44 @@
 (function(){
-	var cardSlider = Hammer($('card-slider'), { prevent_default: true })
+	var cardContainer = $('card-container')
+		, cardSlider = window.cardSlider = Hammer($('card-slider'), { prevent_default: true })
 		, slider = cardSlider.element
 		, posX = 0, prevPosX = 0;
 
 	cardSlider.on('drag release swipe', function(evt){
+		var gesture = evt.gesture;
+
 		switch (evt.type) {
 			case 'drag':
 				slider.removeClass('slider-transition');
-				posX = evt.gesture.deltaX + prevPosX;
+				slider.removeClass('swipe-transition');
+				posX = gesture.deltaX + prevPosX;
 				translateX(slider, posX);
+				cardVm.cards.valueHasMutated();
 				break;
+
 			case 'release':
+				var totalWidth = cardVm.windowWidth() - (cardVm.totalCards() * cardVm.cardWidth);
+
+				if (posX > 0) {
+					posX = 0;
+					slider.addClass('swipe-transition');
+					translateX(slider, posX);
+				}
+
+				if (posX < totalWidth) {
+					posX = totalWidth;
+					slider.addClass('swipe-transition');
+					translateX(slider, posX);
+				}
+
 				prevPosX = posX;
+
+				cardVm.cards.valueHasMutated();
 				break;
+
 			case 'swipe':
 				slider.addClass('slide-transition');
-				posX = ((evt.gesture.deltaX * Math.max(1, Math.min(4, evt.gesture.velocityX))) + prevPosX);
+				posX = ((gesture.deltaX * Math.max(1, Math.min(4, gesture.velocityX))) + prevPosX);
 				translateX(slider, posX);
 				prevPosX = posX;
 				break;
@@ -24,6 +47,8 @@
 
 	slider.addEvent('transitionend', function() {
 		slider.removeClass('slide-transition');
+		slider.removeClass('swipe-transition');
+		cardSlider.trigger('release', {});
 	});
 
 	// Add active to cards
@@ -33,12 +58,15 @@
 	function CardViewModel() {
 		var self = this;
 		window.addEvent('resize', function(){
-			self.windowWidth(window.innerWidth);
+			self.windowWidth(cardContainer.offsetWidth);
 		});
 
 		this.loaderUrl = "public/img/loader.gif";
 		this.cards = ko.observableArray(window.cards);
-		this.windowWidth = ko.observable(window.innerWidth);
+		this.windowWidth = ko.observable(cardContainer.offsetWidth);
+		this.startIdx = ko.observable(0);
+		this.endIdx = ko.observable(1);
+
 		this.totalCards = function() { return this.cards().length; };
 
 		this.totalVisibleCards = function() {
@@ -48,11 +76,15 @@
 		this.getCardInfo = function(el) {
 			if (!this.cardWidth) {
 				this.cardWidth = el.offsetWidth + parseFloat(el.getComputedStyle('margin-right'));
-				var total = this.totalVisibleCards();
-				this.startIdx = -Math.round(posX / this.cardWidth);
-				this.endIdx = this.startIdx + total;
-				this.endIdx = (this.endIdx > this.totalCards() ? this.totalCards() : this.endIdx);
 			}
+
+			var total = this.totalVisibleCards()
+				, startIdx = -Math.round(posX / this.cardWidth)
+				, endIdx = startIdx + total;
+			
+			this.startIdx(startIdx);
+			this.endIdx(endIdx > this.totalCards() ? this.totalCards() : endIdx);
+
 			return this.cardWidth;
 		};
 
@@ -63,7 +95,7 @@
 		};
 
 		this.isActive = function(idx) {
-			if (idx >= this.startIdx && idx <= this.endIdx) {
+			if (idx >= this.startIdx() && idx <= this.endIdx()) {
 				return true;
 			}
 
@@ -75,7 +107,9 @@
 		};
 	}
 
-	ko.applyBindings(new CardViewModel());
+	var cardVm = window.cardVm = new CardViewModel();
+
+	ko.applyBindings(cardVm);
 
 	function translateX(el, posX) {
 		el.style.webkitTransform = 'translate3d('+posX+'px, 0, 0)';
